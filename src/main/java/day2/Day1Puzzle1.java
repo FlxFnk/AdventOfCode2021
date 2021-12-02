9b0a23cae035c8c9b965e1a2739483a3edc71c60
Felix Funke <felix.funke@eckcellent-it.de>
2021-12-02 16:07:21 +0100

    Added day2.

---
+++ src/main/java/day2/Day1Puzzle1.java
@@ -0,0 +1,35 @@
+package day2;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class Day1Puzzle1 {
+	public static void main(String[] args) throws IOException, URISyntaxException {
+		List<String> lines = Files.readAllLines(Paths.get(Day1Puzzle1.class.getResource("input.txt").toURI()));
+
+		int x = 0;
+		int d = 0;
+		int a = 0;
+		for (String line : lines)  {
+			String[] step = line.split(" ");
+			int v = Integer.parseInt(step[1]);
+			switch (step[0]) {
+			case "forward":
+				x += v;
+				d += v * a;
+				break;
+			case "up":
+				a -= v;
+				break;
+			case "down":
+				a += v;
+				break;
+			}
+		}
+		
+		System.out.println("x: " + x + ", d: " + d + ", result: " + (x * d));
+	}
+}
---
+++ src/main/java/day2/Day1Puzzle2.java
@@ -0,0 +1,33 @@
+package day2;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class Day1Puzzle2 {
+	public static void main(String[] args) throws IOException, URISyntaxException {
+		List<String> lines = Files.readAllLines(Paths.get(Day1Puzzle2.class.getResource("input.txt").toURI()));
+
+		int x = 0;
+		int d = 0;
+		for (String line : lines)  {
+			String[] step = line.split(" ");
+			int v = Integer.parseInt(step[1]);
+			switch (step[0]) {
+			case "forward":
+				x += v;
+				break;
+			case "up":
+				d -= v;
+				break;
+			case "down":
+				d += v;
+				break;
+			}
+		}
+		
+		System.out.println("x: " + x + ", d: " + d + ", result: " + (x * d));
+	}
+}
