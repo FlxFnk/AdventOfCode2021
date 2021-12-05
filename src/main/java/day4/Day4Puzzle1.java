f3721a4d5ff2b9d72abd566c7389f22f5cd2834a
Felix Funke <funke@walmar.de>
2021-12-05 19:08:11 +0100

    Added day 4.

---
+++ src/main/java/day4/Day4Puzzle1.java
@@ -0,0 +1,122 @@
+package day4;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public class Day4Puzzle1 {
+	private static class Board {
+		String numbers[][] = new String[5][5];
+		boolean state[][] = new boolean[5][5];
+
+		public void setRow(int row, String line) {
+			numbers[row] = line.trim().split("\\s+");	
+		}
+
+		public void draw(String drawnNumber) {
+			for (int row = 0; row < 5; row++) {
+				for (int column = 0; column < 5; column++) {
+					if (numbers[row][column].equals(drawnNumber)) {
+						state[row][column] = true;
+					}
+				}
+			}
+		}
+
+		public boolean isWin() {
+			boolean win = false;
+			for (int row = 0; row < 5; row++) {
+				win = true;
+				for (int column = 0; column < 5; column++) {
+					win &= state[row][column];
+				}
+
+				if (win) {
+					return true;
+				}
+			}
+			for (int column = 0; column < 5; column++) {
+				win = true;
+				for (int row = 0; row < 5; row++) {
+					win &= state[row][column];
+				}
+
+				if (win) {
+					return true;
+				}
+			}
+
+			return false;
+		}
+		
+		public int unmarkedSum() {
+			int sum = 0;
+			for (int row = 0; row < 5; row++) {
+				for (int column = 0; column < 5; column++) {
+					if (!state[row][column]) {
+						sum += Integer.parseInt(numbers[row][column]);
+					}
+				}
+			}
+			
+			return sum;
+		}
+		
+		public String toString() {
+			StringBuffer b = new StringBuffer();
+			for (int row = 0; row < 5; row++) {
+				for (int column = 0; column < 5; column++) {
+					b.append(String.format("%s%3s%s", state[row][column] ? "(" : " ", numbers[row][column], state[row][column] ? ")" : " "));
+				}
+				b.append("\n");
+			}
+			
+			return b.toString();
+		}
+	}
+
+	public static void main(String[] args) throws IOException, URISyntaxException {
+		List<String> lines = Files.readAllLines(Paths.get(Day4Puzzle1.class.getResource("input.txt").toURI()));
+
+		List<String> numbers = Arrays.asList(lines.get(0).split(","));
+		lines.remove(0);
+		lines.remove(0);
+		
+		List<Board> boards = new LinkedList<>();
+		Board board = new Board();
+		int row = 0;
+		for (String line : lines) {
+			if (line.trim().isEmpty()) {
+				boards.add(board);
+				row = 0;
+				board = new Board();
+			} else {
+				board.setRow(row++, line);
+			}
+		}
+		
+		boards.forEach(System.out::println);
+		
+		for (String number : numbers) {
+			boards.forEach(b -> b.draw(number));
+			
+			Optional<Board> winBoard = boards.stream().filter(Board::isWin).findFirst();
+			if (winBoard.isPresent()) {
+				System.out.println(winBoard.get());
+				int unmarkedSum = winBoard.get().unmarkedSum();
+				System.out.println("Sum: " + unmarkedSum + ", last draw: " + number + ", Result: " + (Integer.parseInt(number)*unmarkedSum));
+				break;
+			} else {
+				System.out.println("Number: " + number);
+				boards.forEach(System.out::println);				
+			}
+		}
+	}
+}
